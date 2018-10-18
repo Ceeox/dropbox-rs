2ac9b177fbe83401f035b1d979fe2e8780d2d024
Unknown <ceox4510471@gmail.com>
2018-10-18 20:11:47 +0200

    update dep. and remove unused macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,7 @@
-#[macro_export]
-macro_rules! gen_uri
-{
+// maybe check of valid uris
+// for example files/create_folder_v2 is ok
+// asdf/create_asdf_v2 should create and gen_uri Error
+macro_rules! gen_uri {
 	( $api_class:expr, $($api_func:expr),* ) =>
 	({
 		use BASE_URL;
@@ -29,7 +30,17 @@ macro_rules! gen_upload_uri
 	});
 }
 
-#[macro_export]
-macro_rules! build_object {
-	() => ();
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn gen_uri() {
+		let ref_1 = "https://api.dropboxapi.com/2/files/create_folder_v2";
+		assert_eq!(ref_1, gen_uri!("files", "create_folder_v2"));
+
+		let ref_2 = "https://api.dropboxapi.com/2/files/upload_session/finish_batch/check";
+		assert_eq!(
+			ref_2,
+			gen_uri!("files", "upload_session", "finish_batch", "check")
+		);
+	}
 }
