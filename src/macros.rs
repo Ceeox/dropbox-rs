a57798a805c27d4c1b2f316d5ed23bdc260ea20e
Unknown <ceox4510471@gmail.com>
2018-11-13 14:35:24 +0100

    fix for edition 2018
    in the edition 2018 an macro can't return an Result apparently, so check for errors outside the macro

--- src/macros.rs
+++ src/macros.rs
@@ -11,7 +11,7 @@ macro_rules! gen_uri {
 			func_calls.push_str(&format!("/{}", $api_func));
 		)*
 		format!("{}{}/{}{}", BASE_URL, API_VERSION,
-			$api_class, func_calls).parse::<hyper::Uri>()?
+			$api_class, func_calls).parse::<hyper::Uri>()
 	});
 }
 
@@ -24,7 +24,7 @@ macro_rules! gen_upload_uri {
 			func_calls.push_str(&format!("/{}", $api_func));
 		)*
 		format!("{}{}/{}{}", UPLOAD_URL, API_VERSION,
-			$api_class, func_calls).parse::<hyper::Uri>()?
+			$api_class, func_calls).parse::<hyper::Uri>()
 	});
 }
 
@@ -60,12 +60,12 @@ mod tests {
 	#[test]
 	fn gen_uri() {
 		let ref_1 = "https://api.dropboxapi.com/2/files/create_folder_v2";
-		assert_eq!(ref_1, gen_uri!("files", "create_folder_v2"));
+		assert_eq!(ref_1, gen_uri!("files", "create_folder_v2").unwrap());
 
 		let ref_2 = "https://api.dropboxapi.com/2/files/upload_session/finish_batch/check";
 		assert_eq!(
 			ref_2,
-			gen_uri!("files", "upload_session", "finish_batch", "check")
+			gen_uri!("files", "upload_session", "finish_batch", "check").unwrap()
 		);
 	}
 }
